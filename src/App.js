c82c080cc5f4ab482c32d70901fb28a3184d6c17
angelbeat <koleso.vitiy@yandex.ru>
2023-10-04 17:37:27 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import logo from './logo.svg';
+import logo from './img/logo.png'
 import './App.css';
 import pepe from './img/pepe.gif'
+import anime from './img/anime1.png'
+
 
 function App() {
   return (
-    <div className='justify-center flex flex-col items-center gap-3 bg-black'>
-      <div className='mt-12 justify-center flex flex-col items-center text-white'>
-        <p className='font-bold text-[300%]'>CNTNT</p>
-        <p className=''>AI контентмейкинг для ваших проектов</p>
+    <div className='flex flex-col justify-center'>
+      <div className='justify-center flex flex-col items-center text-black bg-white w-[100%]'>
+        <p className='font-bold text-[300%] bg-white'>CNTNT</p>
+        <p className='bg-white pb-4'>Контентмейкинг для ваших проектов</p>
       </div>
-      <div className='justify-center flex flex-col items-center mt-3'>
-        <img src={pepe} className='w-[50%]'/>
-      </div>
-      <div className='justify-center flex flex-col items-center gap-3 sm:mt-3 mt-10'>
-        <a href='https://t.me/contentbotoff' className='border-2 border-sky-300 rounded pl-11 pr-11 pt-2 pb-2 text-sky-300 font-bold'>Telegram бот</a>
-        <a href='https://t.me/contentbotoff' className='border-2 border-sky-300 rounded pl-9 pr-9 pt-2 pb-2 text-sky-300 font-bold'>Telegram канал</a>
 
-        {/* class="inline-block rounded border-2 border-primary px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-primary-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:border-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10" */}
-
+      <div className='flex justify-center'>
+        <div className=''>
+          <img src={anime}/>
+        </div>
       </div>
-      <div className='justify-center flex flex-col items-center text-gray-300 sm:mt-3 mt-20'>
-        <p>Разработка началась с 25.09.2023</p>
+
+      <div className='flex flex-col  text-center text-white'>
+        <div className='pl-9 pr-9 pt-3'>
+        Все просто. Вы подключаете ваши каналы к боту, в котором настраиваете "пул", откуда бот будет брать контент для каждого вашего канала, далее вы можете подключить GPT и бот начнет переписывать эти посты в оригинальные, в фишки также входит: добавление вотерки, расписание постинга, вотерка на видео/фото и регулярная техподдержка
+        </div>
+      </div>
+
+      <div className='flex flex-col justify-center items-center'>
+        <div className='justify-center flex flex-col items-center '>
+          <img src={logo} className='w-[65%]'/>
+        </div>
+        <div className='justify-center flex flex-col items-center gap-3 pb-9'>
+          <a href='https://t.me/contentbotoff' className='border-2 border-white rounded pl-9 pr-9 pt-2 pb-2 text-white font-bold hover:border-purple-400 hover:text-purple-400'>Telegram канал</a>
+        </div>
       </div>
     </div>
   );
